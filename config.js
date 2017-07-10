e2d8b2823421d039ec6ee23a6771a70fc5cbdc97
LarissaMorrell <larissa@larissamorrell.com>
2017-07-10 15:50:01 -0400

    mlab link

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+                      'mongodb://dbuser:password@ds143539.mlab.com:43539/blog-app';
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://dbuser:dbpassword@ds151202.mlab.com:51202/blog-app-test');
